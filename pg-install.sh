211f5f22c67cfcd8702a024c033ef472f2a32285
Terho Uotila <terho.uotila@helsinki.fi>
2020-10-12 14:59:39 +0300

    reorder checks

--- pg-install.sh
+++ pg-install.sh
@@ -88,8 +88,8 @@ make install-strip
 
 # update profile file
 
-if grep -q "$INSTALLDIR" "$PROFILEFILE" 2>/dev/null; then
 if [ -f "$PROFILEFILE" ]; then
+if grep -q "$INSTALLDIR" "$PROFILEFILE" 2>/dev/null; then
 echo "LD_LIBRARY_PATH=$INSTALLDIR/lib
 export LD_LIBRARY_PATH
 PATH=$INSTALLDIR/bin:$PATH
@@ -102,6 +102,11 @@ echo "
 Added environment variables to $PROFILEFILE
 "
 else
+echo "
+$PROFILEFILE not updated, $INSTALLDIR is already mentioned there, so assuming this is reinstall and it is up to date.
+"
+fi
+else
 echo "$PROFILEFILE does not exist!"
 echo "LD_LIBRARY_PATH=$INSTALLDIR/lib
 export LD_LIBRARY_PATH
@@ -113,11 +118,6 @@ PGDATA=$INSTALLDIR/data
 export PGDATA" >> "$INSTALLDIR/README.environment"
 echo "
 Added environment variables to $INSTALLDIR/README.environment, copy these manually to correct location.
-"
-fi
-else
-echo "
-$PROFILEFILE not updated, $INSTALLDIR is already mentioned there, so assuming this is reinstall and it is up to date.
 "
 fi
 
